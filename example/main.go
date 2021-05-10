c9a7b3c2be701023fbcf76ee16e400a943d40e4c
abdulahwahdi <abdulah.wahdi@gmail.com>
2021-05-10 13:13:20 +0700

    Update SDK - Add SDK Update Tracking and Update Payload Checkout

--- example/main.go
+++ example/main.go
@@ -8,19 +8,22 @@ import (
 )
 
 const (
-    BaseURL        = "YOUR_BASE_URL"
-    Authorization  = "YOUR_API_KEY"
+    BaseURL        = "https://sandbox.kreditplus.com/hub-api"
+    Authorization  = "SEdtxXLQSDRCeTaIHXWISotSUmdFxzUE"
     DefaultTimeout = 10 * time.Second
 )
 
 func main() {
     newKP := kreditplus.New(BaseURL, Authorization, DefaultTimeout)
-    getCheckoutURL(newKP)
+    GetCheckoutURL(newKP)
+    //UpdateOrderTracking(newKP)
 }
 
-func getCheckoutURL(kp kreditplus.KreditplusService) {
+func GetCheckoutURL(kp kreditplus.KreditplusService) {
     var checkoutReq kreditplus.CheckoutRequest
-    payload := `{"prospect_id": "ORDER_TEST_456", "shipping_cost": 7000, "merchant_url": "https://bhinneka.com", "mobile_phone": "081212341234", "assets": [{"product_id": "NA", "asset_type": "WG", "asset_code": "PHABLET", "category_code": "TEST", "category_name": "PHONE TABLET", "discount_amount": 20000, "dp_amount": 0, "insurance_amount": 0, "item_description": "test", "otr": 1500000, "quantity": 1}]}`
+
+
+    payload := `{"prospect_id": "ORDER_TEST_45679", "shipping_cost": 7000, "merchant_url": "https://bhinneka.com", "mobile_phone": "081212341234", "assets": [{"product_id": "NA", "asset_type": "WG", "asset_code": "PHABLET", "category_code": "TEST", "category_name": "PHONE TABLET", "discount_amount": 20000, "dp_amount": 0, "insurance_amount": 0, "item_description": "test", "otr": 1500000, "quantity": 1}],"shipping_address": { "receiver_name":"test", "address":"test ini khusus alamat buat test aja", "mobile_phone":"081212345678"}}`
     _ = json.Unmarshal([]byte(payload), &checkoutReq)
 
 
@@ -32,6 +35,19 @@ func getCheckoutURL(kp kreditplus.KreditplusService) {
 
     fmt.Println(checkoutResponse)
 }
+func UpdateOrderTracking(kp kreditplus.KreditplusService) {
+    var UpdateOrderTracking kreditplus.UpdateTrackingRequest
+    payload := `{"prospect_id": "ORDER_TEST_45678","awb_number":"test1234551244", "logistic_name":"JNE", "tracking_status":"Barang Dalam Pengiriman"}`
+    _ = json.Unmarshal([]byte(payload), &UpdateOrderTracking)
 
+
+    updateTrackingResp, errUpdateTracking := kp.UpdateTrackingStatus(UpdateOrderTracking)
+    if errUpdateTracking != nil {
+        fmt.Println(errUpdateTracking.Error())
+        return
+    }
+
+    fmt.Println(updateTrackingResp)
+}
 
 
--- service.go
+++ service.go
@@ -2,4 +2,5 @@ package kreditplus
 
 type KreditplusService interface {
     GetCheckoutURL(param CheckoutRequest) (resp Response, err error)
+    UpdateTrackingStatus(param UpdateTrackingRequest) (resp Response, err error)
 }
